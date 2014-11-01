97edd3d40d82131d53f3be2b32668f57c6b43cc8
Ravi Kalaga <ravikalaga@gmail.com>
2014-11-01 09:20:23 -0700

    Remove these 4 files

--- ex2.rb
+++
@@ -1,9 +0,0 @@
-# A comment, this is so you can read your program later. 
-# Anything after the # is ignored by Ruby. 
-
-puts " I could have code like this. " # and the comments like this is good for code but is ignored while displaying. 
-
-# You can also use a comment to "disable" or comment out a piece of code:
-# puts "This won't run"
-
-puts "this will run"
