ec68ba828129e4cabf7b64cab908f0ed5897ee63
Ravi Kalaga <ravikalaga@gmail.com>
2014-10-31 18:09:04 -0700

    new file:   ex1.rb 	new file:   ex2.rb 	new file:   ex3.rb 	new file:   ex4.rb

---
+++ ex1.rb
@@ -0,0 +1,9 @@
+puts "Hello World!"
+puts "Hellow Again!"
+puts "I like typing this!"
+puts "This is fun!"
+puts "Yay, Printing!!"
+puts " I'd must rather you 'not'."
+puts 'I "said" do not touch this'
+puts 'I said do not touch this'
+# puts "Don't print this line"
